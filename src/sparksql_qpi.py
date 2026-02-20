52e568acdbe804f085972367fc30a2ac447bf8ea
ragavan-web <ragavendhra21@gmail.com>
2026-02-20 18:13:12 +0530

    sparksql_using_api

---
+++ src/sparksql_qpi.py
@@ -0,0 +1,24 @@
+
+
+import os
+
+os.environ["PYSPARK_PYTHON"] = r"C:\Users\Mohan\AppData\Local\Programs\Python\Python311\python.exe"
+os.environ["PYSPARK_DRIVER_PYTHON"] = r"C:\Users\Mohan\AppData\Local\Programs\Python\Python311\python.exe"
+
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("DataFrame Example").getOrCreate()
+
+data = [("Mohan", 23), ("Priya", 21)]
+
+df = spark.createDataFrame(data, ["Name", "Age"])
+df.createOrReplaceTempView("students")
+
+spark.sql("SELECT * FROM students WHERE Age > 21").show()
+
+df.show()
+
+
+
+
